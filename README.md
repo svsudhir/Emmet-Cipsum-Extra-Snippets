2a4788612330ccfbfc1eba522434833956c466d8
sv.sudharshan <esves@live.com>
2013-11-05 23:27:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+#Emmet plugin extras for Web developers
+
+Cipsum.js - Provides Corporate jargon words for use as filler text. Usage - type cipsum20 and hit Tab or expand abbreviation from Emmet menu to get 20 random corporate jargon words.
+
+snippets-extra.json - Provides short-cuts for colorscheme generated color styles. Primary, Secondary A, Secondary B and Complimentary.  View this file to see available snippets. It is self-explanatory.
+
+I use Susy Grid, so some additional snippets related to Susy Mixins are also available in this file.
+
+##Install from URL
+
+Download the zip file. 
+
+Windows OS:
+
+Then extract the contents into your Users\<your name>\AppData\Roaming\Notepad++\plugins\config\emmet folder.
+
+Linux and MAC:
+
+Extract the files to your shared folder or wherever you wish and specify the path in your Editor.
+
+##Caution
+
+If you already have snippets-extra.json, then copy the contents of this file and paste it into your file appropriately.
+
+##Special Thanks
+
+To Sergey Chikuyonok, creator of Emmet and other contributors who empower it.  [Emmet is here.](http://emmet.io "Emmet")
---
+++ cipsum.js
@@ -0,0 +1,181 @@
+/**
+ * "Corporate ipsum" text generator. 
+ * By Sudharshan S Veeravalli.
+ * Matches <code>cipsum(num)?</code> or 
+ * <code>clorem(num)?</code> abbreviation.
+ * This code is based on Django's contribution: 
+ * https://code.djangoproject.com/browser/django/trunk/django/contrib/webdesign/lorem_ipsum.py
+ * <br><br>
+ * Examples to test:<br>
+ * <code>cipsum</code> – generates 30 words text.<br>
+ * <code>cipsum*6</code> – generates 6 paragraphs (autowrapped with &lt;p&gt; element) of text.<br>
+ * <code>ol>cipsum10*5</code> — generates ordered list with 5 list items (autowrapped with &lt;li&gt; tag)
+ * with text of 10 words on each line<br>
+ * <code>span*3>cipsum20</code> – generates 3 paragraphs of 20-words text, each wrapped with &lt;span&gt; element .
+ * Each paragraph phrase is unique   
+ * @param {Function} require
+ * @param {Underscore} _ 
+ * @constructor
+ * @memberOf __loremIpsumGeneratorDefine
+ */
+emmet.define('cipsum', function(require, _) {
+	var langs = {
+		en: {
+			common: [''],
+			words: ['proactively','20','24/7','access','action','actualize','adaptive','administrate','after','and','applications','appropriately','architect','architectures','areas','art','authoritatively','b2b','b2c','backend','backward-compatible','base','based','before','benefits','best-of-breed','bricks-and-clicks','business','capital','chains','channels','clicks-and-mortar','client-centered','client-centric','client-focused','collaboration','collaboratively','communicate','communities','compellingly','competencies','competently','competitive','completely','conceptualize','content','continually','conveniently','convergence','coordinate','core','corporate','cost','create','credibly','cross','cross-media','cross-unit','cultivate','customer','customized','cutting-edge','data','deliver','deliverables','deploy','develop','directed','distinctive','distinctively','distributed','diverse','dramatically','dynamically','e-business','e-commerce','e-markets','e-services','e-tailers','effective','efficient','efficiently','embrace','emerging','empower','empowered','enabled','end-to-end','energistically','engage','enthusiastically','envisioned','envisioneer','equity','evisculate','evolve','excellent','experiences','expertise','extensible','extensive','fabricate','focused','for','forward','front-end','fully','functional','future-proof','generate','globally','go','goal-oriented','granular','grow','growth','high','high-payoff','highly','holistic','holistically','holisticly','idea-sharing','ideas','impactful','imperatives','improvements','in','incentivize','inexpensive','infomediaries','information','initiate','initiatives','innovate','innovation','innovative','installed','integrated','intellectual','interactive','interactively','interdependent','interfaces','intermandated','intrinsicly','invested','items','leadership','linkage','magnetic','maintain','maintainable','maintainablea','manufactured','market-driven','markets','materials','maximize','maximizing','mesh','meta-services','methodologies','metrics','mindshare','models','multidisciplinary','multifunctional','multimedia','networks','niche','objectively','of','one-to-one','open-source','opportunities','optimal','orchestrate','orthogonal','out-of-the-box','outside-the-box','outsourcing','paradigms','parallel','partnerships','performance','phosfluorescently','platforms','plug-and-play','pontificate','portals','potentialities','predominate','premier','proactive','procedures','process','process-centric','procrastinate','productize','products','professional','professionally','progressive','progressively','prospective','provide','quality','quickly','rapidiously','rather','re-engineer','real-time','recaptiualize','reinvent','relationships','reliable','repurpose','resource','resource-leveling','resources','restore','results','revolutionary','robust','roi','scale','scenarios','schemas','seamlessly','seize','service','services','simplify','solutions','standardized','standards','state','sticky','strategic','strategies','strategize','sucking','superior','supply','synergistic','synergistically','synergize','synergy','systems','target','task','technology','tested','testing','than','the','theme','thinking','through','timely','to','top-line','total','transform','transition','turnkey','underwhelm','uniquely','unleash','users','utilize','value','value-added','vertical','via','viral','vis-a-vis','visionary','visualize','vortals','web','web-enabled','web-readiness','whereas','whiteboard','wireless','with','without']
+		}
+	};
+
+	var prefs = require('preferences');
+	prefs.define('lorem.defaultLang', 'en');
+
+	/**
+	 * @param {AbbreviationNode} tree
+	 */
+	require('abbreviationParser').addPreprocessor(function(tree) {
+		var re = /^(?:clorem|cipsum)([a-z]{2})?(\d*)$/i, match;
+		
+		/** @param {AbbreviationNode} node */
+		tree.findAll(function(node) {
+			if (node._name && (match = node._name.match(re))) {
+				var wordCound = match[2] || 30;
+				var lang = match[1] || prefs.get('lorem.defaultLang') || 'en';
+				
+				// force node name resolving if node should be repeated
+				// or contains attributes. In this case, node should be outputed
+				// as tag, otherwise as text-only node
+				node._name = '';
+				node.data('forceNameResolving', node.isRepeating() || node.attributeList().length);
+				node.data('pasteOverwrites', true);
+				node.data('paste', function(i) {
+					return paragraph(lang, wordCound, 0);
+				});
+			}
+		});
+	});
+	
+	/**
+	 * Returns random integer between <code>from</code> and <code>to</code> values
+	 * @param {Number} from
+	 * @param {Number} to
+	 * @returns {Number}
+	 */
+	function randint(from, to) {
+		return Math.round(Math.random() * (to - from) + from);
+	}
+	
+	/**
+	 * @param {Array} arr
+	 * @param {Number} count
+	 * @returns {Array}
+	 */
+	function sample(arr, count) {
+		var len = arr.length;
+		var iterations = Math.min(len, count);
+		var result = [];
+		while (result.length < iterations) {
+			var randIx = randint(0, len - 1);
+			if (!_.include(result, randIx))
+				result.push(randIx);
+		}
+		
+		return _.map(result, function(ix) {
+			return arr[ix];
+		});
+	}
+	
+	function choice(val) {
+		if (_.isString(val))
+			return val.charAt(randint(0, val.length - 1));
+		
+		return val[randint(0, val.length - 1)];
+	}
+	
+	function sentence(words, end) {
+		if (words.length) {
+			words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
+		}
+		
+		return words.join(' ') + (end || choice('?!...')); // more dots that question marks
+	}
+	
+	/**
+	 * Insert commas at randomly selected words. This function modifies values
+	 * inside <code>words</code> array 
+	 * @param {Array} words
+	 */
+	function insertCommas(words) {
+		var len = words.length;
+		var totalCommas = 0;
+		
+		if (len > 3 && len <= 6) {
+			totalCommas = randint(0, 1);
+		} else if (len > 6 && len <= 12) {
+			totalCommas = randint(0, 2);
+		} else {
+			totalCommas = randint(1, 4);
+		}
+
+		_.each(_.range(totalCommas), function(ix) {
+			if (ix < words.length - 1) {
+				words[ix] += ',';
+			}
+		});
+	}
+	
+	/**
+	 * Generate a paragraph of "Corporate ipsum" text
+	 * @param {Number} wordCount Words count in paragraph
+	 * @param {Boolean} startWithCommon Should paragraph start with common 
+	 * "Corporate ipsum" sentence.
+	 * @returns {String}
+	 */
+	function paragraph(lang, wordCount, startWithCommon) {
+		var data = langs[lang];
+		if (!data) {
+			return '';
+		}
+
+		var result = [];
+		var totalWords = 0;
+		var words;
+		
+		wordCount = parseInt(wordCount, 10);
+		
+		if (startWithCommon && data.common) {
+			words = data.common.slice(0, wordCount);
+			if (words.length > 5)
+				words[4] += ',';
+			totalWords += words.length;
+			result.push(sentence(words, '.'));
+		}
+		
+		while (totalWords < wordCount) {
+			words = sample(data.words, Math.min(randint(3, 12) * randint(1, 5), wordCount - totalWords));
+			totalWords += words.length;
+			insertCommas(words);
+			result.push(sentence(words));
+		}
+		
+		return result.join(' ');
+	}
+
+	return {
+		/**
+		 * Adds new language words for Corporate ipsum generator
+		 * @param {String} lang Two-letter lang definition
+		 * @param {Object} data Words for language. Maight be either a space-separated 
+		 * list of words (String), Array of words or object with <code>text</code> and
+		 * <code>common</code> properties
+		 */
+		addLang: function(lang, data) {
+			if (_.isString(data)) {
+				data = {words: _.compact(data.split(' '))};
+			} else if (_.isArray(data)) {
+				data = {words: data};
+			}
+
+			langs[lang] = data;
+		}
+	};
+});
